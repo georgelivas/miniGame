3dfcebd2c1365a2e5470e770b92813a6e1fde117
George Livas <giorgos.livas.99@gmail.com>
2017-01-05 20:26:20 +0200

    Initial Commit

---
+++ miniGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miniGame
+//
+//  Created by Giorgos livas on 05/01/2017.
+//  Copyright © 2017 Giorgos livas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
